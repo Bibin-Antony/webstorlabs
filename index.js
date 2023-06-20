d2da0a4337d1170d66dab443982b5526b7860c67
Webstorlabs <webstorlabs@gmail.com>
2023-06-21 01:31:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let textLogo = document.querySelector(".textLogo");
 let container = document.querySelector(".container");
 let webstorTagline = document.querySelector(".webstorTagline");
 let construction = document.querySelector(".construction");
+let webstorBirthday = document.querySelector(".birthday");
 
 setInterval(() => {
   textLogo.style.opacity = "1";
@@ -13,7 +14,11 @@ setInterval(() => {
   webstorTagline.style.transition = "all 0.8s";
 }, 1000);
 setInterval(() => {
+  webstorBirthday.style.opacity = "1";
+  webstorBirthday.style.transition = "all 0.8s";
+}, 1700);
+setInterval(() => {
   construction.style.opacity = "1";
   construction.style.transition = "all 1s";
   document.body.style.transition = "1s";
-}, 2000);
+}, 1900);
