65b9e3d93ac0b4377548ca477e3e1716ab86088f
Webstor-Labs <137144285+Webstor-Labs@users.noreply.github.com>
2023-06-20 17:36:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let textLogo = document.querySelector(".textLogo");
+let container = document.querySelector(".container");
+let webstorTagline = document.querySelector(".webstorTagline");
+let construction = document.querySelector(".construction");
+
+setInterval(() => {
+  textLogo.style.opacity = "1";
+  textLogo.style.transition = "all 0.8s";
+}, 500);
+
+setInterval(() => {
+  webstorTagline.style.opacity = "1";
+  webstorTagline.style.transition = "all 0.8s";
+}, 1000);
+setInterval(() => {
+  construction.style.opacity = "1";
+  construction.style.transition = "all 0.8s";
+  document.body.style.backgroundColor = "white";
+  document.body.style.transition = "1s";
+}, 2000);
